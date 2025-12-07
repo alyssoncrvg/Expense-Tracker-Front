899e54746a2458554daba56add7df85f47b226ac
alyssoncrvg <alyssonpereira41@gmail.com>
2025-12-07 19:24:22 -0300

    feat: implement login component with form validation and authentication
    - Added login.html for the login form structure.
    - Created login.css for styling the login component.
    - Implemented login.ts with reactive form handling and authentication logic.
    - Added unit tests for the login component in login.spec.ts.

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,55 @@
+import { Component, inject, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
+
+  errorMessage = '';
+  isLoading = false;
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({ email: email!, password: password! })
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          this.cdr.detectChanges();
+        })
+      )
+      .subscribe({
+        next: () => {
+          alert('Login bem-sucedido!');
+          // this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          this.errorMessage = err.error?.message || err.message || 'Ocorreu um erro desconhecido.';
+          this.cdr.detectChanges();
+        }
+      });
+  }
+}
