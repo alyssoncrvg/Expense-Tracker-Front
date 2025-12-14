5dc7f0ab2df28fceb291b5896ab07c4e3c0fa9dc
alyssoncrvg <alyssonpereira41@gmail.com>
2025-12-14 10:47:27 -0300

    feat: enhance auth interceptor with token refresh logic and error handling; add ApiResponse model for consistent API responses

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,9 +1,49 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { switchMap, catchError, throwError } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+import { AuthService } from '../services/auth/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const token = localStorage.getItem('access_token');
 
+  if (req.url.includes('/auth/login') || req.url.includes('/auth/refresh')) {
+    return next(req);
+  }
+
   if (token) {
+    try {
+      const decoded = jwtDecode<{ exp: number }>(token);
+      const expirationDate = decoded.exp * 1000;
+      const currentDate = Date.now();
+      
+      const isExpired = currentDate > (expirationDate - 10000);
+
+      if (isExpired) {
+        return authService.refreshToken().pipe(
+          switchMap((response) => {
+            const newReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${response.accessToken}`
+              }
+            });
+            return next(newReq);
+          }),
+          catchError((error) => {
+            authService.logout();
+            return throwError(() => error);
+          })
+        );
+      }
+    } catch (error) {
+      console.error('Token inválido no Storage (formato incorreto). Realizando logout.');
+      authService.logout();
+      
+      // Retorna um erro para cancelar a requisição atual imediatamente
+      return throwError(() => new Error('Token inválido ou corrompido'));
+    }
+
     const clonedReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -1,9 +1,10 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, tap, catchError, throwError } from 'rxjs';
+import { Observable, tap, catchError, throwError, map } from 'rxjs';
 import { Router } from '@angular/router';
 import { LoginRequest, LoginResponse } from '../../models/auth.model';
 import { environment } from '../../../environments/environment';
+import { ApiResponse } from '../../models/apiResponse.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,25 +19,36 @@ export class AuthService {
   constructor() {
     const accessToken = localStorage.getItem('access_token');
     if (accessToken) {
-      console.log('Token encontrado no localStorage durante a inicialização do AuthService.');
       this.currentUser.set({ accessToken, refreshToken: '' });
     }
   }
 
   login(credentials: LoginRequest): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, credentials).pipe(
-      tap((response) => {
-        localStorage.setItem('access_token', response.accessToken);
-        localStorage.setItem('refresh_token', response.refreshToken);
-        this.currentUser.set(response);
+    return this.http.post<ApiResponse<LoginResponse>>(`${this.apiUrl}/login`, credentials).pipe(
+      map((response) => response.data),
+      tap((data) => {
+        localStorage.setItem('access_token', data.accessToken);
+        localStorage.setItem('refresh_token', data.refreshToken);
+        this.currentUser.set(data);
       }),
       catchError((err: HttpErrorResponse) => {
         let errorMessage = 'Ocorreu um erro inesperado';
         if (err.error && err.error.message) {
           errorMessage = err.error.message;
         }
-
         return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
+
+  refreshToken(): Observable<LoginResponse> {
+    const refreshToken = localStorage.getItem('refresh_token');
+    return this.http.post<ApiResponse<LoginResponse>>(`${this.apiUrl}/refresh`, { refreshToken }).pipe(
+      map((response) => response.data),
+      tap((data) => {
+        localStorage.setItem('access_token', data.accessToken);
+        localStorage.setItem('refresh_token', data.refreshToken);
+        this.currentUser.set(data);
       })
     );
   }
